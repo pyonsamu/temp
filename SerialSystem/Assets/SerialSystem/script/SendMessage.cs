32e33486da7a6e36b2d440c24b8e6a8c2e9ab397
pyonsamu <35159755+pyonsamu@users.noreply.github.com>
2020-10-05 18:50:21 +0900

    SerialSystem

---
+++ SerialSystem/Assets/SerialSystem/script/SendMessage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendMessage : MonoBehaviour
+{
+
+    public GameObject SerialHandler;
+    public GameObject InputMessage;
+    public GameObject input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick()
+    {
+        Text text = InputMessage.GetComponent<Text>();
+        Text inputtext = input.GetComponent<Text>();
+        text.text = inputtext.text;
+        Debug.Log(text.text);
+        SerialHandler sh = SerialHandler.GetComponent<SerialHandler>();
+        sh.Write(text.text);
+    }
+}
